5e79009f54e1014bd746d54b420b17632762f9f3
Jibon <jayed.swe@gmail.com>
2017-03-24 23:26:09 +0600

    Add files via upload
    Python variable declaration

---
+++ Python_Variable/Values to Variables.py
@@ -0,0 +1,47 @@
+
+#*****  Variables are nothing but reserved memory locations to store values  ************
+
+# the equal sign (=) is used to assign values to variables
+
+# For example:-
+
+
+a = 'Jibon ' # here a variable and  assign values is ' Jibon '
+print(a)
+
+a = 69
+print(a)
+
+b = 78.36
+print(b,'\n')
+
+
+#**********************    Multiple Assignment  *********************
+
+# Python allows you to assign a single value to several variables simultaneously (একসঙ্গে /Together )
+
+# For example:-
+
+print('Multiple Assignment : ')
+a = b = c = 1
+
+print(a)
+print(b)
+print(c) # same output
+
+
+# Another Example
+
+print('Another Example Multiple Assignment ')
+a,b,c = 1,2,"jibon"
+
+print(a)
+print(b)
+print(c)
+
+
+
+
+
+
+
