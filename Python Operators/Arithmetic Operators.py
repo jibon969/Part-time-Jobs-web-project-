97516ad54583a9543e70da7288852636216a66e0
Jibon <jayed.swe@gmail.com>
2017-03-26 13:56:13 +0600

    Add files via upload
    Arithmetic Operators 
    Comparison (Relational) Operators,
     Assignment Operators ,
    Logical Operators
    Bitwise Operators

---
+++ Python Operators/Arithmetic Operators.py
@@ -0,0 +1,113 @@
+#*******************    Python Basic Operators  *******************
+
+''' Types of Operator '''
+
+'''
+Python language supports the following types of operators.
+
+Arithmetic Operators
+
+Comparison (Relational) Operators
+
+Assignment Operators
+
+Logical Operators
+
+Bitwise Operators
+
+Membership Operators
+
+Identity Operators
+
+'''
+
+#*****************   Python Arithmetic Operators  ***************
+
+#  Show Example :
+
+#*********   + Addition  ************
+
+a = 10 ; b = 20
+
+c = a + b #  Addition
+
+print('Addition 10+20  = ' , c)
+
+#*********  - Subtraction  *************
+
+a = 10 ; b = 20
+
+c = a - b #  Subtraction
+
+print('Subtraction 10-20  = ' ,c)
+
+
+#**********   * Multiplication   *************
+
+a = 10 ; b = 20
+
+c = a * b #  Multiplication
+
+print('Multiplication 10*20 = ' ,c)
+
+
+#***************   / Division   ****************
+
+a = 3 ; b = 10
+
+c = a / b #  Division
+
+print('Division 3 / 10 = ' ,c)
+
+
+#*************    % Modulus   ***********
+
+#  Modulus returns remainder
+
+a = 3 ; b = 10
+
+c = a % b #  Modulus
+
+print('Modulus 3 % 10  = ' ,c)
+
+
+#********    ** Exponent  ************
+
+# declear Exponent a ** b # Must be **
+
+# ( power ) calculation on operators
+
+a = 3 ; b = 5
+
+c = a ** b #  Exponent
+
+print('Exponent 3 ** 5  = ' ,c)
+
+
+#************  Floor Division //  *************
+
+# Floor Division - The division of operands where the result is the quotient
+
+# Example of Floor Division
+
+
+a = 2 // 5    #  EFloor Division
+
+b = 9//2      #  EFloor Division
+
+c = 9.0//2.0   #  EFloor Division
+
+d = -11//3    #  EFloor Division
+
+e = -11.0//3  #  EFloor Division
+
+print('Floor Division  2 // 5   = ' ,a)
+print('Floor Division    9//2   = ' ,b)
+print('Floor Division 9.0//2.0  = ' ,c)
+print('Floor Division -11//3    = ' ,d)
+print('Floor Division -11.0//3  = ' ,e)
+
+
+
+#_______________ Best of luck__________________#
+
---
+++ Python Operators/Bitwise Operators.py
@@ -0,0 +1,37 @@
+#**************   Bitwise Operators **************
+
+'''
+
+Bitwise operator works on bits and performs bit by bit operation.
+
+Assume if a = 60; and b = 13; Now in binary format they will be as follows −
+
+a = 0011 1100
+
+b = 0000 1101
+
+'''
+
+print('5 & 6 : ',5 & 6) # & Binary AND
+
+print('5 | 6 : ', 5 | 6) # | Binary OR
+
+print('5 ^ 6 : ', 5 ^ 6) # ^ Binary XOR
+
+print('\n')
+
+
+#******** More example of  Bitwise Operators **************
+
+
+a = 7
+
+b = 6
+
+print('7 & 6 and 7 | 6 and 7 ^ 6 :\n')
+
+print('7 & 6 :' , a&b)
+
+print('7 | 6 :' ,a|b)
+
+print('7 ^ 6 :' ,a^b)
