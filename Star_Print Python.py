25fc55c7a5f24a8490793fac6936881435cc0601
Jibon <jayed.swe@gmail.com>
2017-03-30 02:07:40 +0600

    Add files via upload
    Star print in Python

---
+++ Star_Print Python.py
@@ -0,0 +1,34 @@
+# Class 7 Solve the problem
+
+# Now write a program user print star from user wish. Example is given below
+
+'''
+    *
+    **
+    ***
+    ****
+    *****
+
+'''
+a = int(input('Enter a number : '))
+for i in range(a+1):
+    for j in range(i):
+        print('*', end=' ')
+    print()
+
+ # Now write a program print star  from user wish. Example is given below
+'''
+*****
+****
+***
+**
+*
+'''
+
+# Code :
+num = int(input('Enter a nubmer : '))
+for i in range(num+1):
+    for j in range(i-1, num-1):
+        print('*', end=' ')
+    print()
+
