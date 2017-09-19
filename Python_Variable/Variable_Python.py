3804f90594116e3a418197e1022e9788bb50cacc
Jibon <jayed.swe@gmail.com>
2017-09-19 09:08:59 +0600

    Add files via upload
    Variable

---
+++ Python_Variable/Variable_Python.py
@@ -0,0 +1,217 @@
+
+#*****  Variables are nothing but reserved memory locations to store values  ************
+
+# the equal sign (=) is used to assign values to variables
+
+# For example:-
+
+'''
+
+a = 'Jibon ' # here a variable and  assign values is ' Jibon '
+
+print(a)
+
+'''
+
+# Example
+
+'''
+a = 10; b = 20; c = 30;
+
+print(a);
+
+print(b);
+
+print(c)
+
+'''
+
+#**********************    Multiple Assignment  *********************
+
+# Python allows you to assign a single value to several variables simultaneously (একসঙ্গে /Together )
+
+# For example:-
+
+'''
+print('Multiple Assignment : ')
+a = b = c = 1
+print(a)
+print(b)
+print(c) # same output
+
+'''
+
+# Another Example
+
+'''
+print('Another Example Multiple Assignment ')
+
+a,b,c = 1,2,"jibon"
+
+print(a)
+
+print(b)
+
+print(c)
+
+'''
+
+
+# Example
+
+'''
+
+one = 1
+
+two = 2
+
+three = one + two
+
+print(three)
+
+# Example
+
+hello = 'Hello'
+
+word = "word"
+
+print(hello+" "+word)
+
+'''
+
+# More Example in python
+
+'''
+a , b, c  = 7, 3, 10
+
+print(a, b, c)
+
+'''
+# More Example 2
+
+'''
+my_float = 9.0
+
+print(my_float)
+
+my_float = float(69)
+
+print(my_float)
+
+'''
+
+# Strings are defined either with a single quote or a double quotes
+
+'''
+
+jibon = "i don't like football"
+
+print(jibon)
+
+'''
+
+# Example
+
+'''
+# it's not work beacuse str and int
+one = 1
+two = 2
+jibon = "Jibon" 
+print(one + two + (jibon))
+
+'''
+
+# Memory locations
+
+'''
+# How to kmown Memory locations in python
+
+a = 12
+
+print(a)
+
+print(id(a)) # Here id () memory locations
+
+'''
+
+
+# Data types
+
+'''
+
+a = 10
+
+print(a)
+
+print(type(a))
+
+b = 20.20
+
+print(b)
+
+print(type(b))
+
+c = 'Jayed'
+
+print(c)
+
+print(type(c))
+
+
+d = 199997777.7777777777
+
+print(d)
+
+print(type(d))
+
+e = 9.j
+
+print(e)
+
+print(type(e))
+
+f = 2^43
+
+print(f)
+
+print(type(f))
+
+'''
+
+# Type cast in python
+
+# Type cast means change one data type to another data type
+
+# Example type cast
+
+a = 12
+
+print(a)
+
+print(type(a))
+
+print("cast float value : ", float(a))
+
+# Another Example of type cast
+
+float = 454545.63636
+
+print(float)
+
+print(type(float))
+
+print("cast int value : ",int(float))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
